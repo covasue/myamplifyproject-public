62ac66897a6bf318eaa3db3da1f264d919a6d9e7
sueyoshi <sueyoshi_keita@st-cova.co.jp>
2022-03-01 21:13:34 +0000

    Set up frontend

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import Amplify from 'aws-amplify'
+import awsExports from './aws-exports'
+Amplify.configure(awsExports)
 createApp(App).mount('#app')
