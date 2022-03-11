254b986d265a0fa043e1f9302bc8175866e67bfc
sueyoshi <sueyoshi_keita@st-cova.co.jp>
2022-03-11 09:39:25 +0000

    With setup()

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         <h1>Todo App</h1>
         <input type="text" v-model="name" placeholder="Todo name" />
         <input type="text" v-model="description" placeholder="Todo description" />
-        <button v-on:click="createTodo">Create Todo</button>
+        <button v-on:click="clickCreateTodo">Create Todo</button>
         <div v-for="item in todos" :key="item.id">
           <h3>{{ item.name }}</h3>
           <p>{{ item.description }}</p>
@@ -17,57 +17,43 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
 import { API } from 'aws-amplify';
 import { createTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
 import { onCreateTodo } from './graphql/subscriptions';
 import { Authenticator } from '@aws-amplify/ui-vue';
 import '@aws-amplify/ui-vue/styles.css';
-export default {
-  name: 'app',
-  async created() {
-    this.getTodos();
-    this.subscribe();
-  },
-  data() {
-    return {
-      name: '',
-      description: '',
-      todos: []
-    }
-  },
-  components: {
-    Authenticator
-  },
-  methods: {
-    async createTodo() {
-      const { name, description } = this;
-      if (!name || !description) return;
-      const todo = { name, description };
-      await API.graphql({
-        query: createTodo,
-        variables: { input: todo },
-      });
-      this.name = '';
-      this.description = '';
-    },
-    async getTodos() {
-      const todos = await API.graphql({
-        query: listTodos
-      });
-      this.todos = todos.data.listTodos.items;
-    },
-    subscribe() {
-      API.graphql({ query: onCreateTodo })
-        .subscribe({
-          next: (eventData) => {
-            let todo = eventData.value.data.onCreateTodo;
-            if (this.todos.some(item => item.name === todo.name)) return; // remove duplications
-            this.todos = [...this.todos, todo];
-          }
-        });
-    }
-  }
+const name = ref(''),
+  description = ref(''),
+  todos = ref([]);
+const clickCreateTodo = async () => {
+  if (!name.value || !description.value) return;
+  const todo = { name: name.value, description: description.value };
+  await API.graphql({
+    query: createTodo,
+    variables: { input: todo },
+  });
+  name.value = '';
+  description.value = '';
 };
+const getTodos = async () => {
+  const todosData = await API.graphql({
+    query: listTodos
+  });
+  todos.value = todosData.data.listTodos.items;
+};
+const subscribe = () => {
+  API.graphql({ query: onCreateTodo })
+    .subscribe({
+      next: (eventData) => {
+        let todo = eventData.value.data.onCreateTodo;
+        if (this.todos.some(item => item.name === todo.name)) return; // remove duplications
+        this.todos = [...this.todos, todo];
+      }
+    });
+};
+getTodos();
+subscribe();
 </script>
